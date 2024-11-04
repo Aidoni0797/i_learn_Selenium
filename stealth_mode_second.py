a53f1a4c4b1ed18e7a14ba04511088456d0bb0ac
Ai Doni <30952073+Aidoni0797@users.noreply.github.com>
2024-11-04 23:38:44 +0600

    stealth_mode_second

---
+++ stealth_mode_second.py
@@ -0,0 +1,18 @@
+# код работает без файла coordinates.crx и aiDoni не знает где его получить, плохо очень плохо
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options_chrome = webdriver.ChromeOptions()
+options_chrome.add_argument('--headless=chrome')
+# options_chrome.add_extension('coordinates.crx') где мне найти этот файл
+
+
+
+
+with webdriver.Chrome(options=options_chrome) as browser:
+    url = 'https://yandex.ru/'
+    browser.get(url)
+    time.sleep(5)
+    a = browser.find_element(By.TAG_NAME, 'a')
+    print(a.get_attribute('href'))
