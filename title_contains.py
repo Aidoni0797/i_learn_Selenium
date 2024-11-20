ed48c0d5f2c3b252cf2bb25d2bcec5fdcfad4f33
Ai Doni <30952073+Aidoni0797@users.noreply.github.com>
2024-11-21 00:51:01 +0600

    title_contains

---
+++ title_contains.py
@@ -0,0 +1,8 @@
+# title_is заработает когда частично совподает с title_contains
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+with webdriver.Chrome() as browser:
+    browser.get('http://parsinger.ru/expectations/2/index.html')
+    element = WebDriverWait(browser, 10).until(EC.title_contains('tle'))
+    print(element)
